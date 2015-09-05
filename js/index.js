c9db2999f75e45207c7367e72cc6915083a96330
zosokh <zoso-kh@jcom.home.ne.jp>
2015-09-05 19:27:41 +0900

    edit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var remote = require('remote');
-var fileUtil = remote.require('../lib/fileUtil');
+var fileUtil = remote.require('./lib/fileUtil');
 
 fileUtil.fetchReadmeList(function(err, matches) {
   if(!err) document.write(matches.join());
